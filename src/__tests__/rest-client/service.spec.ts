82689951d29544a042258d40cf4c4623db314f0c
Faizan-Shiraz <faizan.shiraz@arbisoft.com>
2019-05-12 23:39:40 +0500

    tests adde for put and delete

--- src/__tests__/rest-client/service.spec.ts
+++ src/__tests__/rest-client/service.spec.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line:no-import-side-effect
 import 'isomorphic-fetch';
-import { basicGet, basicPost } from '../../rest-client';
+import { basicGet, basicPost, basicPut, basicDelete } from '../../rest-client';
 
 describe('Restfull Services Test', () => {
   it('BasicGet: should return data for github abc user', () => {
@@ -13,6 +13,20 @@ describe('Restfull Services Test', () => {
 
   it('BasicPost: should return data for github abc user', () => {
     const result = basicPost('http://dummy.restapiexample.com/api/v1/create')({
+      name: 'restful-test', salary: '1111', age: '11',
+    });
+    expect(result).toBeDefined();
+  });
+
+  it('BasicPut: will update the data against user ID 719', () => {
+    const result = basicPut('http://dummy.restapiexample.com/api/v1/update/719')({
+      name: 'restful-test', salary: '1111', age: '11',
+    });
+    expect(result).toBeDefined();
+  });
+
+  it('BasicDelete: should delete user against ID 719', () => {
+    const result = basicDelete('http://dummy.restapiexample.com/api/v1/update/719')({
       name: 'restful-test', salary: '1111', age: '11',
     });
     expect(result).toBeDefined();
